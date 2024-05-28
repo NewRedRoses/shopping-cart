b2e796d91005a986354ee3a4c32b796c6e163482
Mark A. <mark001616@gmail.com>
2024-05-27 20:54:02 -0400

    Add & load Header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from "./Logo";
+const Header = () => {
+  return (
+    <>
+      <Logo />
+    </>
+  );
+};
+
+export default Header;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,8 @@
+import Header from "../Components/Header";
 const Home = () => {
   return (
     <>
+      <Header />
       <h1>Welcome to the Elegant Mock Store</h1>
       <button>Browse Items</button>
     </>
