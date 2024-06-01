cc271cb5f9c51aac013bb162a57802b8817149b2
Mark A. <mark001616@gmail.com>
2024-06-01 14:22:59 -0400

    Add stylized API products

---
+++ src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,16 @@
+import styles from "./ProductCard.module.css";
+const ProductCard = ({ imgUrl, name, price }) => {
+  return (
+    <div className={styles.cardContainer}>
+      <div className="imgContainer">
+        <img className={styles.productImg} src={imgUrl} alt="" />
+      </div>
+      <h2 className={styles.name}>{name}</h2>
+      <span className={styles.priceCartContainer}>
+        <b className={styles.priceContainer}>${price}</b>
+        <button type="button">Add to Cart</button>
+      </span>
+    </div>
+  );
+};
+export default ProductCard;
