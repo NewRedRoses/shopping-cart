5449a1d7e8d2146d56168ece04b53607c5fad6c9
Mark A. <mark001616@gmail.com>
2024-06-01 13:17:02 -0400

    Navigate to home on logo click

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import Logo from "../Logo";
 import { ShoppingCart } from "lucide-react";
 import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
+
       <ShoppingCart color="#2D3436" />
     </div>
   );
