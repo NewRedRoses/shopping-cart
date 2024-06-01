fa5e5a96967ad1858c100277bf25398b35e632d8
Mark A. <mark001616@gmail.com>
2024-06-01 14:15:32 -0400

    Add API & populate card content

--- src/Pages/ListOfItems/ListOfItems.jsx
+++ src/Pages/ListOfItems/ListOfItems.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 import Header from "../../Components/Header/Header";
+import ProductCard from "../../Components/ProductCard/ProductCard";
+import styles from "./ListOfItems.module.css";
 const ListOfItems = () => {
+  const [productData, setProductData] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProductData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <p>Loading...</p>; // Loadig screen for when fetching data
+
   return (
     <>
       <Header />
-      Lorem
+      <h1 className={styles.mainText}>List of All Items</h1>
+      <ul className={styles.cardsContainer}>
+        {productData.map((product) => (
+          <li key={product.id}>
+            <ProductCard
+              name={product.title}
+              imgUrl={product.image}
+              price={product.price}
+            />
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
