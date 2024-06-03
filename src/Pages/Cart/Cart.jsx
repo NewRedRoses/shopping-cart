56d9da5bcc08e213669f216019e23b2e026dd596
Mark A. <mark001616@gmail.com>
2024-06-02 20:31:33 -0400

    Add headline text and basic render list

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,8 +1,19 @@
 import Header from "../../Components/Header/Header";
+import { mainText } from "../../App.module.css";
+import styles from "./Cart.module.css";
 const Cart = () => {
+  const dummyData = ["one", "two", "three", "four"];
   return (
     <>
       <Header />
+      <h1 className={mainText}>All Items on Cart</h1>
+      <ul className={styles.cartContainer}>
+        {dummyData.map((product) => (
+          <li key={product} className={styles.cartItem}>
+            {product}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
