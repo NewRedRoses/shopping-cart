6aeceea59870c3f2044f7c8b080e064abbac0b7b
Mark A. <mark001616@gmail.com>
2024-06-07 21:38:10 -0400

    Destruct props & add ID prop

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,15 +1,27 @@
 import styles from "./ProductCard.module.css";
 import Button from "../Button/Button.jsx";
-const ProductCard = ({ imgUrl, name, price }) => {
+const ProductCard = ({ id, imgUrl, name, price }) => {
+  const {
+    cardContainer,
+    imgContainer,
+    productImg,
+    priceCartContainer,
+    priceContainer,
+  } = styles;
   return (
-    <div className={styles.cardContainer}>
-      <div className={styles.imgContainer}>
-        <img className={styles.productImg} src={imgUrl} alt="" />
+    <div className={cardContainer}>
+      <div className={imgContainer}>
+        <img className={productImg} src={imgUrl} alt="" />
       </div>
       <h2 className={styles.name}>{name}</h2>
-      <span className={styles.priceCartContainer}>
-        <b className={styles.priceContainer}>${price}</b>
-        <Button title="Add to Cart" bgColor="#2D3436" fgColor="#FFFFFF" />
+      <span className={priceCartContainer}>
+        <b className={priceContainer}>${price}</b>
+        <Button
+          title="Add to Cart"
+          bgColor="#2D3436"
+          fgColor="#FFFFFF"
+          onClick={() => console.log(id)}
+        />
       </span>
     </div>
   );
