771756b3fd7db79a09674d94bd5c33eab7ed334d
Mark A. <mark001616@gmail.com>
2024-06-16 13:19:30 -0400

    Add alt text support to component

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,9 +1,10 @@
 import styles from "./Button.module.css";
-const Button = ({ title, bgColor, fgColor, onClick }) => {
+const Button = ({ title, bgColor, fgColor, onClick, alt }) => {
   return (
     <>
       <button
         type="button"
+        alt={alt}
         style={{ backgroundColor: bgColor, color: fgColor }}
         className={styles.btn}
         onClick={onClick}
