bcc3ac815188fa5959e724308a13212bb27f9fde
Mark A. <mark001616@gmail.com>
2024-06-18 21:30:15 -0400

    Remove unnecessary state usage

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,15 +2,15 @@ import Logo from "../Logo";
 import { ShoppingCart } from "lucide-react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-const Header = ({ cartItems, setCartItems }) => {
+const Header = ({ cartItems }) => {
   return (
     <div className={styles.header}>
-      <Link to="/" state={{ cartItems: cartItems }}>
+      <Link to="/">
         <Logo />
       </Link>
 
       <div className={styles.rightContainer}>
-        <Link to="/Cart" state={{ cartItems: cartItems }}>
+        <Link to="/Cart">
           <ShoppingCart className color="#2D3436" />
         </Link>
         {cartItems.length != 0 && (
