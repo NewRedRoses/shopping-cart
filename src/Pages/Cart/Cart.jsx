29c1e31deb885013991a3815c922000e98f795ad
Mark A. <mark001616@gmail.com>
2024-06-21 00:23:12 -0400

    Add conditional rendering if cart's empty

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import CustomInput from "../../Components/CustomInput/CustomInput";
 import styles from "./Cart.module.css";
 import Button from "../../Components/Button/Button.jsx";
 import { useOutletContext } from "react-router-dom";
-import { SquareX } from "lucide-react";
+import { Frown, SquareX } from "lucide-react";
 const Cart = () => {
   const [cartItems, setCartItems] = useOutletContext();
   const [total, setTotal] = useState(0);
@@ -21,54 +21,84 @@ const Cart = () => {
     setTotal(calcCartPriceTotal(newCartItems));
   };
 
+  const emptyCartMsg = () => {
+    return (
+      <>
+        <div className={styles.emptyCartMsgContainer}>
+          <Frown />
+          <span> The cart is empty...</span>
+        </div>
+      </>
+    );
+  };
+
   return (
     <div>
       <h1 className={mainText}>All Items on Cart</h1>
-      <ul className={styles.cartContainer}>
-        {cartItems.map((product, index) => (
-          <li key={index} className={styles.cartItem}>
-            <div className={styles["cart-item-content"]}>
-              <div className={styles.leftSide}>
-                <img
-                  className={styles["img-preview"]}
-                  src={product.imgUrl}
-                  alt=""
-                />
-                <span className={styles.cartTitle}>{product.name}</span>
-                <span className={styles.itemPrice}>${product.price}</span>
-              </div>
-              <span className={styles.quantityContainer}>
-                <CustomInput
-                  className={styles.quantityInput}
-                  type="number"
-                  value={product.quantity}
-                  min={1}
-                  max={100}
-                  setValue={(newQuantity) =>
-                    handleQuantityChange(index, newQuantity)
-                  }
-                />
+      {cartItems.length === 0 ? (
+        emptyCartMsg()
+      ) : (
+        <>
+          <div className={styles.mainContent}>
+            <ul className={styles.cartContainer}>
+              {cartItems.map((product, index) => (
+                <li key={index} className={styles.cartItem}>
+                  <div className={styles["cart-item-content"]}>
+                    <div className={styles.leftSide}>
+                      <img
+                        className={styles["img-preview"]}
+                        src={product.imgUrl}
+                        alt=""
+                      />
+                      <span className={styles.cartTitle}>{product.name}</span>
+                      <span className={styles.itemPrice}>${product.price}</span>
+                    </div>
+                    <span className={styles.quantityContainer}>
+                      <CustomInput
+                        className={styles.quantityInput}
+                        type="number"
+                        value={product.quantity}
+                        min={1}
+                        max={100}
+                        setValue={(newQuantity) =>
+                          handleQuantityChange(index, newQuantity)
+                        }
+                      />
+                      <Button
+                        className={styles.removeBtn}
+                        alt="Delete cart item"
+                        style={{ backgroundColor: "#d63031", padding: "1px" }}
+                        onClick={() => {
+                          setCartItems(
+                            cartItems.filter((a) => a.id !== product.id),
+                          );
+                        }}
+                      >
+                        <SquareX color="#fab1a0" />
+                      </Button>
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className={styles.placeOrderContainer}>
+              <span className={styles.totalPrice}>Total: ${total}</span>
+              <span>
                 <Button
-                  className={styles.removeBtn}
-                  alt="Delete cart item"
-                  style={{ backgroundColor: "#d63031", padding: "1px" }}
-                  onClick={() => {
-                    setCartItems(cartItems.filter((a) => a.id !== product.id));
+                  style={{
+                    backgroundColor: "#2d3436",
+                    color: "#fff",
+                    fontWeight: "bold",
                   }}
+                  className={styles.placeOrderBtn}
                 >
-                  <SquareX color="#fab1a0" />
+                  Place Order
                 </Button>
               </span>
             </div>
-          </li>
-        ))}
-      </ul>
-      <div className={styles.placeOrder}>
-        <span>Total: ${total}</span>
-        <span>
-          <Button>Place Order </Button>
-        </span>
-      </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
